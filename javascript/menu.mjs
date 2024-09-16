3c797d7f81c180e2b0a7eab834309b5b56e106f2
Ivan DiLernia <ivan@datadoghq.com>
2024-09-16 15:55:12 -0400

    Add 'file' context menu scaffolding

--- javascript/menu.mjs
+++ javascript/menu.mjs
@@ -1,15 +1,22 @@
 import { FabricImage } from './lib-fabric.mjs';
 import { sendTxt2Img } from './gradio-adapter.mjs';
-import { ContextMenu } from './menu2.mjs';
 
 class Menu {
   constructor(container) {
     container.innerHTML = `<ul></ul>`;
     this.container = container.querySelector('ul');
+    this.ctxMenu = new ContextMenu();
   }
 
   renderForImage(image, parent) {
     let actions = [];
+
+    actions.push({
+      name: 'File',
+      handler: (target) => {
+        this.ctxMenu.render(target, this.getCtxMenuItems());
+      },
+    });
 
     const src = image.getSrc();
     if (src) {
@@ -36,6 +43,10 @@ class Menu {
         handler: () => {
           image.clipPath = null;
           image.dirty = true;
+          // update the canvas
+          parent.canvas.renderAll();
+          // re-render the menu, as the image might have changed
+          this.render(parent);
         },
       });
     } else {
@@ -43,6 +54,10 @@ class Menu {
         name: 'Crop',
         handler: () => {
           parent.addClipper(image);
+          // update the canvas
+          parent.canvas.renderAll();
+          // re-render the menu, as the image might have changed
+          this.render(parent);
         },
       });
     }
@@ -53,6 +68,10 @@ class Menu {
           image.strokeWidth = 3;
           image.stroke = '#333';
           image.dirty = true;
+          // update the canvas
+          parent.canvas.renderAll();
+          // re-render the menu, as the image might have changed
+          this.render(parent);
         },
       });
     } else {
@@ -61,6 +80,10 @@ class Menu {
         handler: () => {
           image.strokeWidth = 0;
           image.dirty = true;
+          // update the canvas
+          parent.canvas.renderAll();
+          // re-render the menu, as the image might have changed
+          this.render(parent);
         },
       });
     }
@@ -72,7 +95,7 @@ class Menu {
     let actions = [];
     if (parent.canvas.getZoom() !== 1) {
       actions.push({
-        name: '(-) Reset Zoom/Pan',
+        name: 'Reset Zoom/Pan',
         handler: () => {
           parent.resetZoom();
         },
@@ -97,12 +120,8 @@ class Menu {
       const li = document.createElement('li');
       const a = document.createElement('a');
       a.textContent = action.name;
-      a.addEventListener('click', () => {
-        action.handler();
-        // update the canvas
-        parent.canvas.renderAll();
-        // re-render the menu, as the image might have changed
-        this.render(parent);
+      li.addEventListener('click', (e) => {
+        action.handler(li);
       });
       li.appendChild(a);
       this.container.appendChild(li);
@@ -128,7 +147,7 @@ class Menu {
     }
   }
 
-  renderExtraMenu() {
+  getCtxMenuItems() {
     const copyIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" style="margin-right: 7px" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>`;
     const cutIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" style="margin-right: 7px" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="6" cy="6" r="3"></circle><circle cx="6" cy="18" r="3"></circle><line x1="20" y1="4" x2="8.12" y2="15.88"></line><line x1="14.47" y1="14.48" x2="20" y2="20"></line><line x1="8.12" y1="8.12" x2="12" y2="12"></line></svg>`;
     const pasteIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" style="margin-right: 7px; position: relative; top: -1px" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>`;
@@ -138,11 +157,11 @@ class Menu {
     const menuItems = [
       {
         content: `${copyIcon}Copy`,
-        events: {
-          click: (e) => console.log(e, 'Copy Button Click'),
-          // mouseover: () => console.log("Copy Button Mouseover")
-          // You can use any event listener from here
+        handler: (e) => {
+          console.log(e, 'Copy Button Click');
         },
+        // mouseover: () => console.log("Copy Button Mouseover")
+        // You can use any event listener from here
       },
       { content: `${pasteIcon}Paste` },
       { content: `${cutIcon}Cut` },
@@ -153,12 +172,103 @@ class Menu {
       },
     ];
 
-    const dark = new ContextMenu({
-      target: this.container.querySelectorAll('a'),
-      menuItems,
+    return menuItems;
+  }
+}
+
+class ContextMenu {
+  constructor() {
+    this.menuItems = [];
+    this.mode = 'dark';
+    this.target = null;
+    document.addEventListener('click', (event) => {
+      if (!this.target) {
+        return;
+      }
+      if (!this.target.contains(event.target)) {
+        this.clearMenu();
+      }
+    });
+  }
+
+  getMenuItemsNode() {
+    const nodes = [];
+
+    if (!this.menuItems) {
+      console.error('getMenuItemsNode :: Please enter menu items');
+      return [];
+    }
+
+    this.menuItems.forEach((data, index) => {
+      const item = this.createItemMarkup(data);
+      item.firstChild.setAttribute(
+        'style',
+        `animation-delay: ${index * 0.08}s`
+      );
+      nodes.push(item);
     });
 
-    dark.init();
+    return nodes;
+  }
+
+  createItemMarkup(data) {
+    const button = document.createElement('BUTTON');
+    const item = document.createElement('LI');
+
+    button.innerHTML = data.content;
+    button.classList.add('contextMenu-button');
+    item.classList.add('contextMenu-item');
+
+    if (data.divider) item.setAttribute('data-divider', data.divider);
+    item.appendChild(button);
+
+    if (data.handler) {
+      button.addEventListener('click', data.handler);
+    }
+
+    return item;
+  }
+
+  renderMenu() {
+    const menuContainer = document.createElement('UL');
+
+    menuContainer.classList.add('contextMenu');
+    menuContainer.setAttribute('data-theme', this.mode);
+
+    this.getMenuItemsNode().forEach((item) => menuContainer.appendChild(item));
+
+    return menuContainer;
+  }
+
+  clearMenu() {
+    if (!this.contextMenu) {
+      return;
+    }
+    this.contextMenu.remove();
+    this.contextMenu = null;
+  }
+
+  render(target, items) {
+    this.clearMenu();
+    this.menuItems = items;
+    this.target = target;
+
+    const contextMenu = this.renderMenu();
+    this.contextMenu = contextMenu;
+
+    document.body.appendChild(contextMenu);
+
+    const { left, bottom } = target.getBoundingClientRect();
+    const positionY = bottom;
+    const positionX = left;
+
+    contextMenu.setAttribute(
+      'style',
+      `--width: ${contextMenu.scrollWidth}px;
+            --height: ${contextMenu.scrollHeight}px;
+            --top: ${positionY}px;
+            --left: ${positionX}px;`
+    );
   }
 }
 
